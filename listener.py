799e61bfdc1a0bbd5befa2c26cba983b4bc87543
operatorequals <john.torakis@gmail.com>
2016-11-15 02:20:32 +0200

    Added 8 sample files. Created argparse. Commented Code. Cleaned some code.

--- listener.py
+++ listener.py
@@ -1,43 +1,55 @@
+#!/usr/bin/env python
 import os
 from time import sleep
-from sys import stdout
+from sys import stdout, exit
+import argparse
+
 
 ENDLINE = '\x92'
-# print ord(ENDLINE)
-files = ['/tmp/upl.gif', '/tmp/upl2.gif', '/tmp/upl3.gif', '/tmp/upl4.gif']
 
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group( required = True )
+group.add_argument( '--directory', '-d', help = 'Use all files in this directory in the * "ls" returns them (BEWARE: MUST BE WRITABLE)')
+group.add_argument( '--files', '-f', help = 'Use the listed files as in the * they are given', nargs = '*')
 
+args = parser.parse_args()
+# print args
 
+files = args.files
+if args.directory :
+	files = [ (args.directory + x) for x in os.listdir( args.directory ) ]
+
+# print files
 Delivered = False
-message = ''
 
-def tick(file) :
-	return (os.stat(file)[0] % 512) ^ 512 
+def tick(file) :							# Returns the "Tick bit" (First file's Owner Read bit)
+	return (os.stat(file)[0] % 512) ^ 512 	# as boolean
 
 
-last_state = tick(files[0])
+def consume_char( char ) :
+
+		stdout.write(str(char))				# Print Char without newlines
+		stdout.flush()						# Flush the output stream
+
+
+last_state = tick(files[0])					# Gets the state of Tick Bit
 while not Delivered :
 
+	cur_state = tick(files[0])				# Saves the current Tick Bit
+	while cur_state == last_state :			# If the current and previous Tick bits are same
+		sleep (0.2)							# Wait a while
+		cur_state = tick(files[0])			# Get new Tick bit in case it changed
+											# Spin-Lock until Tick bit Changes
 
 
-	cur_state = tick(files[0])
-	# print cur_state
-	while cur_state == last_state :
-		sleep (0.2)
-		cur_state = tick(files[0])
-		# print cur_state,
+	for f in files :						# Tick happened - New data in file privileges!
 
-
-	for f in files :
-
-		mod = (os.stat(f)[0])
-		char = chr(mod % 256)
-		# message += char
-		if char == (ENDLINE) :
-			# print "End<>"
-			Delivered = True
+		mod = (os.stat(f)[0])				# Get the privilege int
+		char = chr(mod % 256)				# Strip it to 1 byte and save it as ASCII Character
+		if char == (ENDLINE) :				# If it is the Special Character
+			Delivered = True				# The Message has been delivered
 			break
-		stdout.write(str(char))
-		stdout.flush()
+
+		consume_char( char )				# Consume char in some way. Default: print it.
 
 	last_state = cur_state
--- stegoer.py
+++ stegoer.py
@@ -1,51 +1,85 @@
-
-import os
+#!/usr/bin/env python
+import os, sys
+import argparse
 from time import sleep
+import signal
 
 
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group( required = True )
+group.add_argument( '--directory', '-d', help = 'Use all files in this directory in the * "ls" returns them (BEWARE: MUST BE WRITABLE)')
+group.add_argument( '--files', '-f', help = 'Use the listed files as in the * they are given', nargs = '*')
+
+parser.add_argument( '--delay', default = 0.5, type = float, help = "Set the delay between the chmod's")
+parser.add_argument( 'message', help = 'Message to be transmitted. Can be the output of a shell command if you use backticks (`) or $() expression in quotes ("")' )
+args = parser.parse_args()
 
 
 ENDLINE = '\x92'
-message = '''TheSuperASCII_Secret! ()!@#$%^&*_+-={}[];'\\:,./<>?~`|\n'''*4
-# message = 'secret'
-message += ENDLINE*4
+# message = '''TheSuperASCII_Secret! ()!@#$%^&*_+-={}[];'\\:,./<>?~`|\n'''*4
+message = args.message
+delay = args.delay
 
-delay = 0.4
-
-files = ['/tmp/upl.gif', '/tmp/upl2.gif', '/tmp/upl3.gif', '/tmp/upl4.gif']
+files = args.files
+if args.directory :
+	files = [ (args.directory + x) for x in os.listdir( args.directory ) ]
 file_num = len(files)
+message += ENDLINE * file_num
 
-initial_status = []
-
+initial_status = []							# Get the Initial State of all files
 for f in files :
 	initial_status.append( {'mod' : os.stat(f)[0], 'a_time' : os.stat(f)[-3], 'm_time' : os.stat(f)[-2], 'c_time' : os.stat(f)[-1]} )
 
+
+
+def revert_chmod() :
+	for f, st in zip(files, initial_status) :
+		os.chmod(f, st['mod'])
+		os.utime(f, (st['a_time'],  st['m_time']) )
+
+signal.signal(signal.SIGSEGV, revert_chmod)
+
+
+
 tick = 1
-def do_tick( file ) :
-	global tick 		# ticking mechanism. XOR with 1 or 0 the MS
-	mod = (os.stat(file)[0])
+def do_tick( file ) :						# Ticking mechanism.
+	global tick 							# Toggle the Owner Read bit of First File in List
+	mod = (os.stat(file)[0])				# 
 	if (tick % 2) :
-		mod |= (1<<8)	# set the R user bit
+		mod |= (1<<8)
 	os.chmod( file, mod )
-	# os.system( 'ls -l %s' % file )
 	tick += 1
 
+MessageEnded = False
+try :
 
-items, chunk = message, file_num
-for c in zip(*[iter(items)]*chunk) :	# http://www.garyrobinson.net/2008/04/splitting-a-pyt.html
-	
-	for char, file in zip(c, files) :
-		mod = ord(char)
-		os.chmod(file, mod)
-		print "{:5}	{:1} {:15}".format(mod, char, file)
-		if char == ENDLINE :
+	items, chunk = message, file_num
+	for c in zip(*[iter(items)]*chunk) :	# http://www.garyrobinson.net/2008/04/splitting-a-pyt.html
+		
+		if MessageEnded :
 			break
 
-	do_tick(files[0])
-	sleep( delay )
+		for char, file in zip(c, files) :	# pick a char and a file
+			mod = ord(char)					# make the char an int
+			os.chmod(file, mod)				# chmod the file with that int
+
+			if char == '\n' :
+				char = '\\n'
+			print "{:5}	{:1} {:15}".format(mod, char, file)
+
+			if char == ENDLINE :			# If we found the special character
+				MessageEnded = True
+				break						# The message is over
+
+		do_tick(files[0])
+		sleep( delay )
 
 
-for f, st in zip(files, initial_status) :
-	print st
-	os.chmod(f, st['mod'])
-	os.utime(f, (st['a_time'],  st['m_time']) )
+
+except KeyboardInterrupt :					# In case of Ctrl - C
+	revert_chmod()							# Revert all files to initial state
+	print 
+	print "Aborted by user..."
+	sys.exit(-1)							# Exit non-zero
+
+revert_chmod()								# Revert files before exiting gracefully
